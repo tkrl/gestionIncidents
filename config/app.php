bf8c4894e7eac16c8246af07cf07cb5efc2e0fc4
tkrl <justinlongin7@gmail.com>
2025-12-20 09:06:08 +0100

    dixième comit

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://gestionincidents-starting.up.railway.app'),
+    // 'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
